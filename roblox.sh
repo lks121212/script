72b66dffd1ca4184b303ae73843caac1b90bf839
lks121212 <vitor.marlow@escola.pr.gov.br>
2026-03-30 19:41:19 +0000

    Update

--- roblox.sh
+++ roblox.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+set -e
+
 flatpak --user remote-add -y --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo >/dev/null 2>&1
-flatpak --user install -y --noninteractive flathub org.vinegarhq.Sober >/dev/null 2>&1
+
+if ! flatpak info org.vinegarhq.Sober >/dev/null 2>&1; then
+  flatpak --user install -y --noninteractive flathub org.vinegarhq.Sober >/dev/null 2>&1
+fi
+
 flatpak run org.vinegarhq.Sober >/dev/null 2>&1 &
