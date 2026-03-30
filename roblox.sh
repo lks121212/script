e5aae903428f83e51e31cdce49e85d4f164d27d2
lks121212 <vitor.marlow@escola.pr.gov.br>
2026-03-30 19:37:22 +0000

    Update

--- roblox.sh
+++ roblox.sh
@@ -1,3 +1,3 @@
 flatpak -y --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak -y --user install flathub org.vinegarhq.Sober
-flatpak run org.vinegarhq.Sober
+flatpak run org.vinegarhq.Sober >/dev/null 2>&1 &
--- steam.sh
+++ steam.sh
@@ -1,3 +1,3 @@
 flatpak -y --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak -y --user install flathub com.valvesoftware.Steam
-flatpak run com.valvesoftware.Steam
+flatpak run com.valvesoftware.Steam >/dev/null 2>&1 &
